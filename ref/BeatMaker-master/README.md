e6b85a0bb053738bbc3289a6ff34279a35632e14
DenizD <edc.deniz@gmail.com>
2024-12-18 15:02:39 +0300

    Folder structure addition :: .py updation

--- ref/BeatMaker-master/README.md
+++
@@ -1,9 +0,0 @@
-# BeatMaker
-
-An app that you can create basses sounds!
-
-Build with pygame library.
-
-To open the app just run the command:
-
-python main.py
--- scripts/bism.py
+++ scripts/bism.py
@@ -4,25 +4,23 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 
-# Define the scope of the required permissions 
-SCOPE = "user-library-read user-top-read" 
-# Define your client ID, client secret, and redirect URI 
-client_id = "73bce3be0fa34320a350e72d2a2cde3b" 
-client_secret = "318c48fdd78544f587c09363ee29a212" 
-redirect_uri = "http://localhost:8080" 
-# Initialize the Spotify client with the OAuth manager 
+# Define the scope of the required permissions
+SCOPE = "user-library-read user-top-read"
+# Define your client ID, client secret, and redirect URI
+client_id = "73bce3be0fa34320a350e72d2a2cde3b"
+client_secret = "318c48fdd78544f587c09363ee29a212"
+redirect_uri = "http://localhost:8080"
+# Initialize the Spotify client with the OAuth manager
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
-                                               client_secret=client_secret, 
-                                               redirect_uri=redirect_uri, 
+                                               client_secret=client_secret,
+                                               redirect_uri=redirect_uri,
                                                scope=SCOPE))
-#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE)) 
-# Now you can use the client to access the user's top tracks 
-results = sp.current_user_top_tracks(limit=10) 
-#for idx, track in enumerate(results['items']): 
+#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
+# Now you can use the client to access the user's top tracks
+results = sp.current_user_top_tracks(limit=10)
+#for idx, track in enumerate(results['items']):
 #  print(f"{idx + 1}. {track['name']} by {track['artists'][0]['name']}")
-  
 ##############################################################
- 
 from mido import Message, MidiFile, MidiTrack
 
 mid = MidiFile()
@@ -36,8 +34,7 @@ track.append(Message('note_on', note=38, velocity=100, time=240))  # Snare
 track.append(Message('note_off', note=38, velocity=64, time=360))
 
 mid.save('C:\\Users\\denizdu\\Documents\\example.mid')
-#mid.save('example.mid') 
-  
+#mid.save('example.mid')
 ##############################################################
 
 from sklearn.tree import DecisionTreeClassifier
